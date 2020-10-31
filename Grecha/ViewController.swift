329108cc1d5b1a51a88bc3a06914045fe69de896
Alexander Telegin <telegin.alexander@gmail.com>
2020-10-31 14:03:09 +0300

    Add icon

--- Grecha/ViewController.swift
+++ Grecha/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let placeholder = UIImageView(image: UIImage(named: "grecha"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        placeholder.add(to: view).do {
+            $0.contentMode = .scaleAspectFit
+            $0.edgesToSuperview(insets: UIEdgeInsets(top: 80, left: 40, bottom: 80, right: 40))
+        }
     }
-
-
 }
-
